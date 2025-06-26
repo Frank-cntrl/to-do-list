1a838b30d5263c8a246578669a51381b598aea1f
Franccesco Petta <franccescopettamuller@gmail.com>
2025-06-26 19:38:27 -0400

    fix vercel link

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
 
   async function fetchAllTasks() {
     try {
-      const response = await axios.get("https://express-intro-starting-point-git-patch-franks-projects-601765f0.vercel.app/tasks/");
+      const response = await axios.get("https://express-intro-starting-point-git-patch-franks-projects-601765f0.vercel.app");
       setTasks(response.data);
     } catch (error) {
       console.error("Error fetching tasks:", error);
